57c1ee95487c76a1dc6b4be0bfc3d998c4aa233b
Jonas Kvinge <jonas@jkvinge.net>
2026-04-15 20:23:31 +0200

    Update copyright

--- src/compareversions.cpp
+++ src/compareversions.cpp
@@ -1,5 +1,6 @@
 /* This file is part of qtsparkle.
    Copyright (c) 2010 David Sansome <me@davidsansome.com>
+   Copyright 2018-2026, Jonas Kvinge <jonas@jkvinge.net>
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
